de575e063f03bfc205659c7698e944b798089b59
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-10-14 19:48:07 +0300

    init commit

---
+++ MoviesApp/MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by ahmed elhawary on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
