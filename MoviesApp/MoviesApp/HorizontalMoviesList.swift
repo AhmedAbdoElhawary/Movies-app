e739d3d2480543ad7a0afbe72a9dbb58df7efd71
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-10-14 22:59:03 +0300

    create HorizontalMoviesList

--- MoviesApp/MoviesApp/HorizontalMoviesList.swift
+++ MoviesApp/MoviesApp/HorizontalMoviesList.swift
@@ -1,63 +1,51 @@
 import SwiftUI
 
 struct HorizontalMoviesList: View {
-    let url = URL(string: "https://picsum.photos/1200")
+    let url:URL?
 
     var body: some View {
-        VStack(spacing: 20) {
-            GroupOfMoviesTitle(title: "Categories", subTitle: "Expand")
-            CategoriesOptions()
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack(spacing: 10) {
+                ForEach(0..<10) { _ in
+                    VStack(alignment: .leading,spacing: 0) {
+                        RoundedPoster(url:url)
 
-            GroupOfMoviesTitle(title: "Latest Movies", subTitle: "View all")
+                        Text("Movie Name")
+                            .font(.system(size: 14, weight: .medium))
+                            .padding(.top,8)
+                            .padding(.bottom,2)
 
-            ScrollView(.horizontal, showsIndicators: false) {
-                LazyHStack(spacing: 10) {
-                    ForEach(0..<10) { _ in
-                        VStack(alignment: .leading,spacing: 0) {
-                            RoundedPoster(url:url)
-
-                            Text("Movie Name")
-                                .font(.system(size: 14, weight: .medium))
-                                .padding(.top,8)
-                                .padding(.bottom,2)
-
-                            HStack(spacing: 2) {
-                                Image(systemName: "clock")
-                                    .font(.system(size: 12))
-                                    .foregroundColor(Color(#colorLiteral(red: 0.4263476563, green: 0.4263476563, blue: 0.4263476563, alpha: 1)))
-                                Text("114 mins")
-                                    .font(.system(size: 12, weight: .medium))
-                                    .foregroundColor(Color(#colorLiteral(red: 0.4263476563, green: 0.4263476563, blue: 0.4263476563, alpha: 1)))
-                                
-                                Spacer()
-                                
-                                Image(systemName: "star.fill")
-                                    .font(.system(size: 12))
-                                    .foregroundColor(Color(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)))
-                                Text("8.5")
-                                    .font(.system(size: 12, weight: .medium))
-                                    .foregroundColor(Color(#colorLiteral(red: 0.4263476563, green: 0.4263476563, blue: 0.4263476563, alpha: 1)))
-                                
-                            }
+                        HStack(spacing: 2) {
+                            Image(systemName: "clock")
+                                .font(.system(size: 12))
+                                .foregroundColor(Color(#colorLiteral(red: 0.4263476563, green: 0.4263476563, blue: 0.4263476563, alpha: 1)))
+                            Text("114 mins")
+                                .font(.system(size: 12, weight: .medium))
+                                .foregroundColor(Color(#colorLiteral(red: 0.4263476563, green: 0.4263476563, blue: 0.4263476563, alpha: 1)))
+                            
+                            Spacer()
+                            
+                            Image(systemName: "star.fill")
+                                .font(.system(size: 12))
+                                .foregroundColor(Color(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)))
+                            Text("8.5")
+                                .font(.system(size: 12, weight: .medium))
+                                .foregroundColor(Color(#colorLiteral(red: 0.4263476563, green: 0.4263476563, blue: 0.4263476563, alpha: 1)))
+                            
                         }
                     }
                 }
-                .frame(height: 245)
-                    .padding(.leading, 10)
-                    .padding(.trailing, 10)
             }
-
+            .frame(height: 245)
+                .padding(.leading, 10)
+                .padding(.trailing, 10)
         }
     }
 }
 
-#Preview {
-    ContentView()
-}
-
 struct RoundedPoster: View {
 
-    let url:URL
+    let url:URL?
     
     var body: some View {
         ZStack {
@@ -90,8 +78,4 @@ struct RoundedPoster: View {
         .cornerRadius(10)
         .clipped()
     }
-}
-
-#Preview {
-    RoundedPoster()
 }
