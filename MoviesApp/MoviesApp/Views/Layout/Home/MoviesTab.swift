5a1b870cc66ff502affd43f217d91a181d24e51c
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-10-15 23:15:50 +0300

    feed MoviesTab with actual data

--- MoviesApp/MoviesApp/Views/Layout/Home/MoviesTab.swift
+++ MoviesApp/MoviesApp/Views/Layout/Home/MoviesTab.swift
@@ -1,28 +1,62 @@
 import SwiftUI
 
 struct MoviesView: View {
-    let url = URL(string: "https://picsum.photos/1200")
-@State private var moviesViewModel = MoviesViewModel()
+    @State private var moviesViewModel = MoviesViewModel()
+    
     var body: some View {
         ScrollView(showsIndicators: false) {
             LazyVStack(spacing: 20) {
                 GroupOfMoviesTitle(title: "Categories", subTitle: "Expand")
                 CategoriesOptions()
 
+                GroupOfMoviesTitle(
+                    title: "Upcoming Movies",
+                    subTitle: "View all"
+                )
+                HorizontalMoviesList(
+                    loadableState: $moviesViewModel.upcomingMoviesState,
+                    initailRequest: {
+                        await moviesViewModel.getUpcomingMovies()
+                    }
+                )
+                .task {
+                    await moviesViewModel.getUpcomingMovies()
+                }
                 GroupOfMoviesTitle(title: "Latest Movies", subTitle: "View all")
-                HorizontalMoviesList(url: url,moviesViewModel:$moviesViewModel)
-                    .task {
+                HorizontalMoviesList(
+                    loadableState: $moviesViewModel.latestMoviesState,
+                    initailRequest: {
                         await moviesViewModel.getLatestMovies()
                     }
-                
+                )
+                .task {
+                    await moviesViewModel.getLatestMovies()
+                }
+
                 GroupOfMoviesTitle(title: "Top Movies", subTitle: "View all")
-                HorizontalMoviesList(url: url,moviesViewModel:$moviesViewModel)
+                HorizontalMoviesList(
+                    loadableState: $moviesViewModel.topRatedMoviesState,
+                    initailRequest: {
+                        await moviesViewModel.getTopRatedMovies()
+                    }
+                )
+                .task {
+                    await moviesViewModel.getTopRatedMovies()
+                }
 
                 GroupOfMoviesTitle(
                     title: "Popular Movies",
                     subTitle: "View all"
                 )
-                HorizontalMoviesList(url: url,moviesViewModel:$moviesViewModel)
+                HorizontalMoviesList(
+                    loadableState: $moviesViewModel.popularMoviesState,
+                    initailRequest: {
+                        await moviesViewModel.getPopularMovies()
+                    }
+                )
+                .task {
+                    await moviesViewModel.getPopularMovies()
+                }
 
             }
         }
