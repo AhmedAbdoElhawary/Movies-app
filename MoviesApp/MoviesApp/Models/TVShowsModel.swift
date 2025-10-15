8cbbf29788355925de10cb28db1d32154489f54d
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-10-15 23:15:08 +0300

    create TVShowsResults

---
+++ MoviesApp/MoviesApp/Models/TVShowsModel.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+//
+//struct TVShowsModel : Codable {
+//    let page : Int?
+//    let results : [TVShowsResults]?
+//    let totalPages : Int?
+//    let totalResults : Int?
+//
+//}
+
+class TVShowsResults: MoviesResults {
+  
+}
